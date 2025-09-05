177ecd19efaac1932d47c09fa999c1d1d4c4b0af
dancdtr <dan@conductr.co.uk>
2025-09-05 12:31:20 +0100

    refactor lines

--- classification/lines.py
+++ classification/lines.py
@@ -101,27 +101,26 @@ def is_line_nnw(g: Gesture) -> bool:
     return _is_line_subintercardinal(g, Azimuth.NNW)
 
 
-def _is_line_cardinal(g: Gesture, primary: Azimuth, variance_deg=_CARDINAL_ANGLE_VARIANCE) -> bool:
-    has_velocity = _has_velocity_in_direction(g, primary)
+def _is_line_cardinal(g: Gesture, target: Azimuth, variance_deg=_CARDINAL_ANGLE_VARIANCE) -> bool:
+    has_velocity = _has_velocity_in_direction(g, target)
+    return _is_line(g, target, variance_deg, has_velocity)
 
-    return _is_line(g, primary=primary, variance_deg=variance_deg, has_velocity=has_velocity)
 
+def _is_line_intercardinal(g: Gesture, target: Azimuth, variance_deg=_INTERCARDINAL_ANGLE_VARIANCE) -> bool:
+    target_1, target_2 = _SPLIT_SUBCARDINALS[target]
+    has_velocity = _has_velocity_in_direction(g, target_1) or _has_velocity_in_direction(g, target_2)
 
-def _is_line_intercardinal(g: Gesture, primary: Azimuth, variance_deg=_INTERCARDINAL_ANGLE_VARIANCE) -> bool:
-    a1, a2 = _SPLIT_SUBCARDINALS[primary]
-    has_velocity = _has_velocity_in_direction(g, a1) or _has_velocity_in_direction(g, a2)
-
-    return _is_line(g, primary=primary, variance_deg=variance_deg, has_velocity=has_velocity)
+    return _is_line(g, target, variance_deg, has_velocity)
 
 
 def _is_line_subintercardinal(g: Gesture, target: Azimuth, variance_deg=_SUBINTERCARDINAL_ANGLE_VARIANCE) -> bool:
-    a1, a2 = _SPLIT_SUBINTERCARDINALS[target]
+    target_1, target_2 = _SPLIT_SUBINTERCARDINALS[target]
 
-    return _is_line_cardinal(g, a1, variance_deg) or _is_line_intercardinal(g, a2, variance_deg)
+    return _is_line_cardinal(g, target_1, variance_deg) or _is_line_intercardinal(g, target_2, variance_deg)
 
 
-def _is_line(g: Gesture, primary: Azimuth, variance_deg: float, has_velocity: bool) -> bool:
-    has_azimuth = has_azimuth_in_range(g, target=primary, variance_deg=variance_deg)
+def _is_line(g: Gesture, target: Azimuth, variance_deg: float, has_velocity: bool) -> bool:
+    has_azimuth = has_azimuth_in_range(g, target, variance_deg)
 
     # print(f"Gesture ({g.id}) {primary.name} line: velocity={has_velocity}, azimuth={has_azimuth}")
     return has_velocity and has_azimuth
@@ -138,7 +137,7 @@ def _has_velocity_in_direction(g: Gesture, azimuth: Azimuth) -> bool:
     # if g._MIN_VELOCITY
 
 
-def has_azimuth_in_range(g: Gesture, target: Azimuth, variance_deg: float = 22.5) -> bool:
+def has_azimuth_in_range(g: Gesture, target: Azimuth, variance_deg) -> bool:
     angle = g.direction.get_azimuth()
     delta = ((angle - target.deg + 180.0) % 360.0) - 180.0
     return abs(delta) <= variance_deg + 1e-9
